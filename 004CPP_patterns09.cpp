f4f201ba8b47c2d8bf3ec6732c0d7840f382610c
Md Tarik Anvar <tarik.22190503033@cuj.ac.in>
2026-02-18 08:48:54 +0530

    Add files via upload

---
+++ 004CPP_patterns09.cpp
@@ -0,0 +1,26 @@
+/*  1
+    12
+    123
+    1234
+    12345
+    */
+
+#include <iostream>
+using namespace std;    
+int main() {
+int n;
+cout << "Enter the number of rows: ";
+cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        int num=1;
+        for (int j = 1; j <= i; j++) {
+            cout <<num <<" ";
+            num++;
+        }
+        
+        cout << endl;
+    }
+
+    return 0;
+}
