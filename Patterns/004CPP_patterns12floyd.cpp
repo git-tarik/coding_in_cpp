18cc6c8702871afc13475ae2d1bad69622cbe4c2
Md Tarik Anvar <tarik.22190503033@cuj.ac.in>
2026-03-11 23:56:39 +0530

    linked list

---
+++ Patterns/004CPP_patterns12floyd.cpp
@@ -0,0 +1,24 @@
+/*  1
+    23
+    456
+    78910
+    */
+
+#include <iostream>
+using namespace std;    
+int main() {
+int n;
+int num=1;
+cout << "Enter the number of rows: ";
+cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout <<num <<" ";
+            num++;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
