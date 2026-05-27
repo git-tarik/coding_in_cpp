7fbb7a7a55dfaac53203997d1cad75c6cc2f7e10
Md Tarik Anvar <tarik.22190503033@cuj.ac.in>
2026-05-27 13:11:56 +0530

    unordered map

---
+++ Non_Sequential_Containers/multimap.cpp
@@ -0,0 +1,16 @@
+#include<iostream>'
+#include<map>
+#include<string>
+
+using namespace std;
+int main(){
+    multimap<string,int> m;
+    m.insert({"tv",100});
+    m.insert({"tv",200});
+    m.insert({"tv",300});
+    m.erase(m.find("tv"));
+    for(auto p:m){
+        cout<<p.first<<" : "<<p.second<<endl;
+    }
+    return 0;
+} 
